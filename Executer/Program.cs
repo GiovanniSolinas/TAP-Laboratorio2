b3cdb9f5593c10fa80c7ee4bcef4cf8042295930
Giovanni Solinas <giovanni.solinas98@gmail.com>
2020-11-06 15:40:30 +0100

    Prove con la classe Activator

--- Executer/Program.cs
+++ Executer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using MyLibrary;
 
 namespace Executer
 {
@@ -14,9 +15,19 @@ namespace Executer
             var a = Assembly.LoadFrom("MyLibrary.dll");
             foreach (var type in a.GetTypes())
             {
-                if(type.IsClass) Console.WriteLine(type.FullName);
+                if (type.IsClass)
+                {
+                    var o = Activator.CreateInstance(type);
+                    Foo pippo = (Foo) o;
+                    var methods = type.GetMethods();
+                    foreach (var method in methods)
+                    {
+                        Console.WriteLine(method.Name);
+                    }
+                }
             }
             Console.ReadLine();
         }
+            
     }
 }
