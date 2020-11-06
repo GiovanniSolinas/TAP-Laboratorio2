1198dc07a99771082b98ed363776c03e901be875
Giovanni Solinas <giovanni.solinas98@gmail.com>
2020-11-06 12:37:20 +0100

    Aggiungere i file di progetto.

---
+++ MyAttribute/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAttribute
+{
+    [AttributeUsage(AttributeTargets.Method,AllowMultiple = true)]
+    public class ExecuteMe : Attribute
+    {
+        private readonly string name;
+        public ExecuteMe(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+    }
+}
